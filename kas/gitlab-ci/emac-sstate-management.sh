7c3fab8220a98c332b690fe7a8edaeffcf64e8b9
Brenden Tisler <btisler@emacinc.com>
2023-05-04 16:11:50 -0500

    incorrect path in new script files, testing sstate
    Signed-off-by: Brenden Tisler <btisler@emacinc.com>

--- kas/gitlab-ci/emac-sstate-management.sh
+++ kas/gitlab-ci/emac-sstate-management.sh
@@ -1,5 +1,5 @@
 eval `bitbake -e | grep ^SSTATE_DIR= | sed 's|\"||g'`
-tail --pid=$(cat build/bitbake.lock) -f /dev/null || true
+tail --pid=$(cat bitbake.lock) -f /dev/null || true
 eval `bitbake -e | grep ^ALL_MULTILIB_PACKAGE_ARCHS= | sed 's|\"||g' | sed 's| |,|g'`
-tail --pid=$(cat build/bitbake.lock) -f /dev/null || true
+tail --pid=$(cat bitbake.lock) -f /dev/null || true
 sstate-cache-management.sh -d -y --cache-dir=$SSTATE_DIR --extra-archs=$ALL_MULTILIB_PACKAGE_ARCHS
