8c2b24c7a50d829da0359cff459c2e1b6928f5b7
Brenden Tisler <btisler@emacinc.com>
2023-05-05 14:34:31 -0500

    ready for merge into kirkstone-next
    Signed-off-by: Brenden Tisler <btisler@emacinc.com>

---
+++ kas/gitlab-ci/emac-deployment.sh
@@ -0,0 +1,8 @@
+eval `bitbake -e | grep ^ARCHIVE_PATH= | sed 's|\"||g'`
+tail --pid=$(cat bitbake.lock) -f /dev/null || true
+export ARCHIVE_PATH=$ARCHIVE_PATH
+eval `bitbake -e | grep ^DEPLOY_DIR= | sed 's|\"||g'`
+tail --pid=$(cat bitbake.lock) -f /dev/null || true
+export DEPLOY_DIR=$DEPLOY_DIR
+upload_oe;
+upload_oe_archive;
