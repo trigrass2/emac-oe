da31caa765d3f7047e8f2e3e9403bd5daa316d52
rchouhan1 <rchouhan@emacinc.com>
2023-07-18 11:45:42 -0400

    u-boot bring-up for ipac9x25

---
+++ meta-emac-bsp/vendor-layers/atmel/recipes-bsp/u-boot/u-boot-at91/emac-sources/include/configs/ipac9x25.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2012 Atmel Corporation
+ *
+ * Configuation settings for the AT91SAM9X5EK board.
+ */
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/* ARM asynchronous clock */
+#define CONFIG_SYS_AT91_SLOW_CLOCK	32768
+
+/* SDRAM */
+#define CONFIG_SYS_SDRAM_BASE		0x20000000
+#define CONFIG_SYS_SDRAM_SIZE		0x08000000	/* 128 megs */
+
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_SDRAM_BASE + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
+
+#endif
