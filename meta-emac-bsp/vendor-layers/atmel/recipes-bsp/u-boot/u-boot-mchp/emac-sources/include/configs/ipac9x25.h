f410f1d6decae3c4062e45df27745376b3273dff
Brenden Tisler <btisler@emacinc.com>
2024-01-04 15:27:03 -0600

    Dont believe this to be needed any more, referencing the sam9x25ek
    Signed-off-by: Brenden Tisler <btisler@emacinc.com>

--- meta-emac-bsp/vendor-layers/atmel/recipes-bsp/u-boot/u-boot-mchp/emac-sources/include/configs/ipac9x25.h
+++ meta-emac-bsp/vendor-layers/atmel/recipes-bsp/u-boot/u-boot-mchp/emac-sources/include/configs/ipac9x25.h
@@ -8,7 +8,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define DEBUG 1
+#define DEBUG 1 // REMOVE THIS
 
 /* ARM asynchronous clock */
 #define CFG_SYS_AT91_SLOW_CLOCK	32768
@@ -26,13 +26,3 @@
 #define CFG_SYS_MCKR_CSS		0x1302
 
 #endif
-
-#undef CFG_DM_USB
-#define ATMEL_BASE_OHCI		0x00600000 /* USB Host controller (OHCI) */
-#define CFG_USB_ATMEL
-#define CFG_USB_ATMEL_CLK_SEL_UPLL
-#define CFG_USB_OHCI_NEW
-#define CFG_SYS_USB_OHCI_CPU_INIT
-#define CFG_SYS_USB_OHCI_REGS_BASE		ATMEL_BASE_OHCI
-#define CFG_SYS_USB_OHCI_SLOT_NAME		"at91sam9x5"
-#define CFG_SYS_USB_OHCI_MAX_ROOT_PORTS	3
