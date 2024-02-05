889c571ec0c9a946afc0afdd54028f294ba552b4
Brenden Tisler <btisler@emacinc.com>
2024-02-05 12:10:56 -0600

    cleaned up ci using latest model
    Signed-off-by: Brenden Tisler <btisler@emacinc.com>

--- ci/scripts/emac-deployment.sh
+++ ci/scripts/emac-deployment.sh
@@ -1,7 +1,7 @@
-eval `bitbake -e | grep ^ARCHIVE_PATH= | sed 's|\"||g'`
+eval `bitbake core-image-base -e | grep ^ARCHIVE_PATH= | sed 's|\"||g'`
 tail --pid=$(cat bitbake.lock) -f /dev/null || true
 export ARCHIVE_PATH=$ARCHIVE_PATH
-eval `bitbake -e | grep ^DEPLOY_DIR= | sed 's|\"||g'`
+eval `bitbake core-image-base -e | grep ^DEPLOY_DIR= | sed 's|\"||g'`
 tail --pid=$(cat bitbake.lock) -f /dev/null || true
 export DEPLOY_DIR=$DEPLOY_DIR
 upload_oe;
